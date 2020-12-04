657c2df4b546f1fa9a30693a5d9ce9550f0347be
Qi Zhu <rubybfm1017@gmail.com>
2020-12-04 14:13:22 -0800

    w3d5 1st turn

--- W3D5/poly_tree_node/lib/00_tree_node.rb
+++ W3D5/poly_tree_node/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
+    attr_reader :parent,:value, :children
+    def initialize(str)
+        @parent = nil
+        @children = []
+        @value = str
+    end
+
+    def parent=(other_node)
+        self.parent = other_node.parent
+        @children = @children << other_node.children
+
+    end
 
 end
