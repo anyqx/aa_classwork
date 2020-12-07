909cdbcd97b06123f88e430a8731dc12b68cdb0d
annieyxu <annieyxu@berkeley.edu>
2020-12-07 14:57:26 -0800

    last step

--- W4D1/knights_travails.rb
+++ W4D1/knights_travails.rb
@@ -1,21 +1,48 @@
+require_relative 'PolyTreeNode'
 class KnightPathFinder
+    attr_reader: start
     def self.valid_moves(pos)
-        @considered_positions = [@start,pos]
-        @possible_moves = []
-        @possible_moves << [row-1][col-2], [row-1][col+2], [row+1][col-2],[row+1][col+2],[row-2][col+1],[row-2][col-1],[row+2][col+1],[row+2][col-1]
+        i, j = pos
+        @possible_moves = [[i-1,j-2], [i-1,j+2], [i+1,j-2], [i+1,j+2],[i-2,j+1],[i-2,j-1],[i+2,j+1],[i+2,j-1]]
+        ## need to consider edge cases where i/j < 2 etc
+
+
+        # filter out positions that are in @considered_positions
+        @considered_positions.each do |ele|
+            @possible_moves.delete(ele) if @considered_positions.include?(ele)
+        end
+        @possible_moves
     end
     
     def initialize(start)
         @start = start#start is a position , eg : [0,0], it is root node
-        
+        @considered_positions = [@start]
     end
 
-    def [](pos)
-        row, col = pos
-        pos[row][col]
+    def new_move_positions(pos)
+        #call self.valid_moves? 
+        #if valid, put the positions in the array, add new positions to considered_positons
+        new_positions = KnightPathFinder.valid_moves(pos)
+        @considered_positions += new_positions
+        new_positions
     end
      
     def build_move_tree
+        #set up a tree
+        root_node = PolyTreeNode.new(@start)
+        queue = [root_node]
+        until queue.empty?
+            current_node = queue.shift
 
+            self.new_move_positions(current_node.value) 
+            #new positions, we will need to turn the positions into nodes
+            #for each node, add_child (parent/child relation)
+            child.children.each do |ele|
+                queue << ele
+            end
+        end
+        nil
+
+        until 
     end
 end
