a5ba8f5e31c9693b08e11d9f4bb6c2d12d3fe513
annieyxu <annieyxu@berkeley.edu>
2021-01-24 22:34:13 -0800

    w8d3 update

--- w8d3/skeleton 10/phase_2_enumerable.js
+++ w8d3/skeleton 10/phase_2_enumerable.js
@@ -1,48 +1,32 @@
+Array.prototype.myEach = function (cb) {
 
-
-Array.prototype.myEach = function(cb) {
-    for (let i = 0; i < this.length; i++){
+    for (let i = 0; i < this.length; i++) {
         cb(this[i]);
     };
 };
 
-let add = function(x,y){
+let add = function (x, y) {
     return x + y;
 };
 // console.log([1,2,3].myEach(add));
 
-Array.prototype.myMap = function(cb) {
-    let mapped = [];
-    this.myEach(function(ele) {
-        let result = cb(ele);
-        mapped.push(result);
-    });
-    return mapped
+Array.prototype.myMap = function (cb) {
+    const map = [];
+    this.myEach(el => map.push(cb(el)));
+    return map;
 };
 
-Array.prototype.myMap2 = function (cb) {
-    let mapped = [];
-    const innerFunction = function (ele) {
-        mapped.push(cb(ele))
-    };
-
-    this.myEach(innerFunction);
-    return mapped;
-};
-
-[1,2,3].myMap2(myDoubler)
-console.log(arr)
 // console.log([1,2,3].myMap(add));
 
-Array.prototype.myReduce = function(cb,initial){
+Array.prototype.myReduce = function (cb, initial) {
     let array = this;
     if (initial === undefined) {
         initial = this[0];
         array = array.slice(1);
     };
     let result = initial;
-    array.myEach(el => result=cb(result, el));
+    array.myEach(el => result = cb(result, el));
     return result;
 };
 
-console.log([1,2,3].myReduce(add, 10));
+console.log([1, 2, 3].myReduce(add, 10));
